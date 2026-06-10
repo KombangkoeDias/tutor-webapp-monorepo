54145caa0f04534dd72b7c06f91f9bd9eacf638f
Sakon Thephamongkhol <kbdirifilation@gmail.com>
2026-06-10 21:15:52 +0800

    optimize

--- apps/tutor-web-app/src/services/http-client.ts
+++ apps/tutor-web-app/src/services/http-client.ts
@@ -5,7 +5,7 @@ export const JWT_TOKEN_KEY = "JWTToken";
 let axiosClient = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_BACKEND_URL ||
-    "http://api.jobtutordream.com/api/v1",
+    "https://api.jobtutordream.com/api/v1",
   timeout: 60000,
   headers: {
     "Content-Type": "application/json",
